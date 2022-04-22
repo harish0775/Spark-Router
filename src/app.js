b42cc85ab5a13fee75035ebc15fe5c70d9d8ff76
harish0775 <harish.nishad0775@gmail.com>
2022-04-23 05:21:51 +0530

    Add Link To React Router:

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 // import React, { Fragment } from 'react';
 // import { render } from "react-dom";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Navbar from "./Navbar";
 function App() { 
     return ( 
         <div className="App">
            <BrowserRouter>
+           <Navbar/>
             <Routes>
-            <Route path = "/" element = {<h1> Home</h1>}/>
-            <Route path = "/about" element = {<h1> about</h1>}/>
-            <Route path = "/contact" element = {<h1> Contact</h1>}/>
+            <Route path = "/" element = {<Home/>}/>
+            <Route path = "/about" element = {<About/>}/>
+            <Route path = "/contact" element = {<Contact/>}/>
             </Routes>
            </BrowserRouter>
         </div>
