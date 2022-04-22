06a04371cdbd0afdea35fb11b5be75bc1fab9fe0
harish0775 <harish.nishad0775@gmail.com>
2022-04-23 01:51:01 +0530

    add file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { StrictMode } from 'react';
 import React, { Fragment } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
+import App from './app';//ewe
 function Hello() {
   return (
     <Fragment>
